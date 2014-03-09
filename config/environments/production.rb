a67aa7a0967ab402eea6b6a61e67c1f59a3ce24c
David Pollak <davidpollak4@gmail.com>
2014-03-08 21:32:05 -0500

    add amazon for image upload to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Pinteresting::Application.configure do
   #required for heroku devise
   #Note to set to actual host name
   config.action_mailer.default_url_options = { :host => 'redpin.heroku.com' }
+# sets paperclip to upload to amazon
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
+
 end
