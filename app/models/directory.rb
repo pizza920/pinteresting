e211f3c8ac57746c1c514553f3cd322612914305
Vitaliy <vbykovec@gmail.com>
2014-07-07 00:07:03 +0300

    update category subdir parent defining, update view

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -5,7 +5,7 @@ class Directory < ActiveRecord::Base
 
   has_many :websites
   scope :roots, -> { where(:level => 0) }
-  scope :no_parent, -> { where(:parent_id => nil).where.not(:level => 0) }
+  scope :no_parent, -> { where(:parent_id => nil).where(:level => nil) }
   default_scope -> {order('position asc')}
   has_many :subdir, class_name: 'Directory', :foreign_key => :parent_id
   belongs_to :parent, class_name: 'Directory'
@@ -13,18 +13,29 @@ class Directory < ActiveRecord::Base
   def self.setparents
     Directory.update_all parent_id: nil
     Directory.roots.order('position asc').each do |root|
+      p root.title
       synonyms = root.synonyms.split(',').map(&:strip).map(&:downcase)
-      Directory.no_parent.where.not(level: 0).find_each do |cat|
+      Directory.no_parent.each do |cat|
+        p cat.id
         synonyms.each do |synonym|
+          p cat.title
+          p synonym
           if cat.title.downcase.match synonym
+            p 'matched!'
             cat.parent_id = root.id
             cat.save
           end
         end
       end
     end
+    Directory.set_sub_count
+    nil
   end
 
   def self.set_sub_count
+    Directory.roots.each  do |root|
+      root.sub_count = root.subdir.count
+      root.save
+    end
   end
 end
